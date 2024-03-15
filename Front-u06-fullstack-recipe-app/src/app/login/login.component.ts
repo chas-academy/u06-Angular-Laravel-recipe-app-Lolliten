c54630902cfcbb3f2b8392e6d17753511febe47f
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-03-15 22:03:54 +0100

    "Login form connected to app comp"

--- Front-u06-fullstack-recipe-app/src/app/login/login.component.ts
+++ Front-u06-fullstack-recipe-app/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  // LoginComponent logic
+
+  @Input() title: string;
+
+  constructor() {
+    this.title = '';
+  }
 
 }
