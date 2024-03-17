645a070bdc0484c571b595229a0d96cbf663fd7a
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-03-17 21:50:47 +0100

    "Clean main"

--- Front-u06-fullstack-recipe-app/src/app/app.component.ts
+++ Front-u06-fullstack-recipe-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Front-u06-fullstack-recipe-app/src/app/login/login.component.ts
+++ Front-u06-fullstack-recipe-app/src/app/login/login.component.ts
@@ -1,19 +1,3 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
-export class LoginComponent {
-  // LoginComponent logic
-
-  @Input() title: string;
-
-  constructor() {
-    this.title = '';
-  }
-
-}
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
