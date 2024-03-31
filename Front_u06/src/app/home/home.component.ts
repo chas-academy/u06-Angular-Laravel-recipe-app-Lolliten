24ec36da7ee1f39e13f88a98743e08ea926a919e
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-03-31 12:56:09 +0200

    "Delete recipesearch(home comp is used for search)"

--- Front_u06/src/app/home/home.component.ts
+++ Front_u06/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Recipeinfo } from '../Interfaces/recipeinfo';
 import { RecipeinfoComponent } from '../recipeinfo/recipeinfo.component';
+import { RecipeService } from '../services/recipe.service';
+
 
 @Component({
   selector: 'app-home',
@@ -28,8 +30,31 @@ export class HomeComponent {
     // Handle form submission logic here
     console.log('Form submitted');
   }
-  
+
 }
+
+  //Delete this?
+  /*recipes?: Recipe;
+
+  searchterm = "chicken";
+
+  constructor(private recipeService: RecipeService) { }
+
+  searchRecipe() {
+    this.recipeService.getRecipe(this.searchterm,).subscribe(result) =>
+    console.log(result);
+    let recipe: Recipe[];
+    recipes = result.hits.map[item => {
+      
+    id: number;
+    title: string;
+    nutrition: string;
+    allergenes: string;
+    photo: string;
+  }
+    }]*/
+  
+
 
  
 
