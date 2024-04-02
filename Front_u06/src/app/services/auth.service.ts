1d48052930fe91899c6930bbc166495aa0aec2ea
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-04-02 22:09:48 +0200

    "fixed auth register"

--- Front_u06/src/app/services/auth.service.ts
+++ Front_u06/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
 import { User } from '../Interfaces/user';
 import { LoginDetails } from '../Interfaces/logindetails';
 import { Registerdetails } from '../Interfaces/registerdetails';
+import { Router } from '@angular/router'; //?
 
 //TAKE THIS AWAY??
 interface ResultData {
@@ -46,52 +47,61 @@ export class AuthService {
 
   //where is resultdata? Do i need to create IF?
   loginUser(loginDetails: LoginDetails){
-    this.http.post<ResultData>(this.baseUrl+'login', loginDetails, this.httpOptions).pipe(
+    this.http.post<any>(this.baseUrl+'login', loginDetails, this.httpOptions).pipe(
       catchError(this.handleError)).subscribe(result => {
         console.log(result);
         this.updateLoginState(true);
         this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer " + result.token);
+        //this.router.navigate([''])
       })
   }
 
-  //handle the subscribe in the app, 
-  /*
-  this.http.get<any/datatype>
-  .subscribe(result => {
-    console.log(result);
-  */
-  //Is this right?
-  register(registerdetails: RegisterDetails){
-    this.http.post<ResultData>(this.baseUrl+'register', registerdetails, this.httpOptions).pipe(
+  register(form: any) {
+    this.http.post<any>(this.baseUrl + 'register', form, this.httpOptions).pipe(
+      catchError(this.handleError)
+    ).subscribe(res => {
+      console.log(res);
+      console.log(res.token);
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer "); // + result.token //should this line be here?
+      //localStorage.setItem("token", res.token);
+      //this.router.navigate(['/login']);
+    });
+  }
+
+  //First try reg, Is this right?
+  /*register(registerdetails: RegisterDetails){
+    this.http.post<RegisterDetails>(this.baseUrl + 'register', registerdetails, this.httpOptions).pipe(
       catchError(this.handleError)).subscribe // ?? 
       //How does the rest look like? 
-     //
-  }
+      /*(??? => {
+        console.log(???)
+      });
+
+  }*/
 
   logOut(){
     this.http.post<ResultData>(this.baseUrl+'logout', {}, this.httpOptions).pipe(
       catchError(this.handleError)).subscribe(result => {
         console.log(result);
         this.updateLoginState(false);
-        this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer ");
+        this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer "); //+ token ??
       })
   }
 
   getUser2(): Observable<User[]> {
+    this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer "); //+ token ??
     return this.http.get<User[]>(this.baseUrl+'getuser/2', this.httpOptions);
   }
 
   private handleError(error: HttpErrorResponse){
     if (error.status === 404) {
-      // A client-side or network error occurred. Handle it accordingly.
+     
       console.error('An error occurred:', error.error);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
     }
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(() => new Error('Something happened, try again later.'));
   }
 
 }
--- Front_u06/src/app/services/recipe.service.ts
+++ Front_u06/src/app/services/recipe.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Filter } from '../Interfaces/filter';
-
+import { Observable } from 'rxjs'; //??
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RecipeService {
 
   private baseUrl = "https://api.spoonacular.com/recipes/complexSearch"
@@ -15,7 +16,7 @@ export class RecipeService {
   private httpOptions = {
     headers: new HttpHeaders({
       'accept': 'application/json',
-      'Accept-Language': 'en'
+      //'Accept-Language': 'en' //Ta bort??
     })
   }
 
